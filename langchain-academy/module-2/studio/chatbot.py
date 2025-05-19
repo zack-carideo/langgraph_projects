11dd83472aefe09b0c4a03fa88a7603b14ce36cd
zack-carideo <zjc1002@gmail.com>
2025-05-19 14:10:57 -0400

    updated with my creds

--- langchain-academy/module-2/studio/chatbot.py
+++ langchain-academy/module-2/studio/chatbot.py
@@ -1,17 +1,51 @@
 from langchain_core.messages import HumanMessage, SystemMessage, RemoveMessage
 from langgraph.graph import MessagesState
 from langgraph.graph import StateGraph, START, END
-
+from typing import Optional, Literal, Dict
 # We will use this model for both the conversation and the summarization
 from langchain_openai import ChatOpenAI
-model = ChatOpenAI(model="gpt-4o", temperature=0) 
+import os, getpass, sys
+from langchain_anthropic import ChatAnthropic
+
+#Import API Keys(storing them all in a seperate file so i run into less issues with git)
+_root = "/home/zjc1002/Mounts/code/"
+_langsmith_trace = 'true'
+_langsmith_project = 'zjc_custom_v1'
+_anthropic_model = 'claude-3-haiku-20240307'
+_temparate = 0
+_max_tokens = 2000
+_streaming = True
+_system_message = None
+
+# This is a basic config to enable tracing of conversations and  utilization of memory
+_config = {'configurable': {'thread_id':"1"}}
+
+# Custom functions
+sys.path.append(_root)
+from  admin.api_keys import _api_keys
+from admin.sys_ops import _set_env
+from langgraph_projects.tools import get_weather, get_date, multiply, add, divide
+
+# This anthropic and internet tool requires  API key to be set as a environment variable(s)
+for api_key in ["TAVILY_API_KEY","ANTHROPIC_API_KEY",'OPENAI_API_KEY','LANGSMITH_API_KEY']:
+    _set_env(api_key
+            ,value =  _api_keys[api_key])
+    
+model = ChatAnthropic(
+    model=_anthropic_model,
+    temperature=_temparate,
+    max_tokens=_max_tokens,
+    streaming=_streaming,
+    verbose=False,
+)
 
 # State class to store messages and summary
 class State(MessagesState):
-    summary: str
+    summary: Optional[str]
     
 # Define the logic to call the model
-def call_model(state: State):
+def call_model(state: State)-> Dict[str, str]:
+    """Call the model with the messages in the state."""
     
     # Get summary if it exists
     summary = state.get("summary", "")
@@ -32,7 +66,8 @@ def call_model(state: State):
     return {"messages": response}
 
 # Determine whether to end or summarize the conversation
-def should_continue(state: State):
+
+def should_continue(state: State) -> Literal["summarize_conversation", "__end__"]:
     
     """Return the next node to execute."""
     
@@ -43,9 +78,9 @@ def should_continue(state: State):
         return "summarize_conversation"
     
     # Otherwise we can just end
-    return END
+    return "__end__"
 
-def summarize_conversation(state: State):
+def summarize_conversation(state: State): 
     
     # First get the summary if it exists
     summary = state.get("summary", "")
@@ -74,11 +109,15 @@ def summarize_conversation(state: State):
 # Define a new graph
 workflow = StateGraph(State)
 workflow.add_node("conversation", call_model)
-workflow.add_node(summarize_conversation)
+workflow.add_node("summarize_conversation", summarize_conversation)
 
 # Set the entrypoint as conversation
 workflow.add_edge(START, "conversation")
-workflow.add_conditional_edges("conversation", should_continue)
+workflow.add_conditional_edges("conversation" # Starting Node: "conversation"
+                               , should_continue # Function to determine next node
+                               , {"summarize_conversation": "summarize_conversation"
+                                  , "__end__": END} # mapping function output to next node
+                                  )
 workflow.add_edge("summarize_conversation", END)
 
 # Compile
